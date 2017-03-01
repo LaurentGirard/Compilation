ffdd285a6f9f278011bd3b725addcadef5ef126e
LaurentGirard <laurent.girard44620@gmail.com>
2017-03-01 12:21:09 +0100

    new scanner in progress

--- compilateur.cpp
+++ compilateur.cpp
@@ -7,6 +7,8 @@ using namespace std;
 
 enum AtomType { Terminal, NonTerminal };
 
+enum Code { ELTER, IDNTER };
+
 enum Operation { Conc, Union, Star, Un, Atom };
 
 struct type_conc{
@@ -44,11 +46,17 @@ struct node{
 	}typeNode;
 };
 
+struct lexical_unit{
+	string chaine;
+	int action;
+	AtomType type;
+	Code code;
+};
+
 vector<char> separators;
 vector<string> symbols;
-vector<string> lexicalUnits;
 vector<string> lexicalErrors;
-vector<int> actions;
+vector<lexical_unit> lexicalUnits;
 
 node* genConc(node* left, node* right){
 	node* n = new node();
@@ -212,8 +220,8 @@ bool isSymbol(char charac){
 
 	if (charac == '+' || charac == '-' || charac == '=' || charac == '*' || charac == '&' || charac == '|' || charac == '/' || 
 		charac == '^' || charac == '(' || charac == ')' || charac == '[' || charac == ']' || charac == '{' || charac == '}' ||
-		charac == ';' || charac == ':' || charac == ',' || charac == '<' || charac == '>' || charac == '\"'||
-		charac == '\''|| charac == '@' || charac == '!' || charac == '?'){
+		charac == ';' || charac == ':' || charac == ',' || charac == '<' || charac == '>' || charac == '\''|| charac == '@' || 
+		charac == '!' || charac == '?'){
 			
 			return true;
 	}
@@ -284,71 +292,170 @@ bool isNumber(string unit){
 	return true;
 }
 
-void scanner(string text){
+int actionOfLexicalUnit(string unit){
 
-	unsigned int i;
+	string action;
+
+	for(int i = 0 ; i < unit.size() ; ++i){
+		action = unit.substr(i+1, unit.size()-1);
+
+		if (unit.at(i) == '#'){
+			if (isNumber(action)){
+				return atoi(action.c_str());
+			}else{
+				return -2;
+			}
+		}
+	}
+
+	return -1;
+}
+
+string rmActionFromLexicalUnit(string unit){
+
+	string unitWithoutAction = "";
+	unsigned int i = 0;
+
+	while( (unit.at(i) != '#') && (i < unit.size()) ){
+		unitWithoutAction += unit.at(i);
+		++i;
+	}
+
+	return unitWithoutAction;
+}
+
+void addLexicalUnit(string chaine, int action, AtomType type, Code code){
+
+	lexical_unit currentLexical;
+
+	currentLexical.chaine = chaine;
+	currentLexical.action = action; // -1 mean no action
+	currentLexical.type = type;
+	currentLexical.code = code;
+
+	lexicalUnits.push_back(currentLexical);
+}
+
+void analyseUnit(string unit){
+
+	int action;
+
+	if (unit != "") {
+
+	if(!isInteger(unit.at(0))){
+
+		// find the action of a string if there is one (-2 for an error, -1 for a unit without code and the code otherwise)
+		action = actionOfLexicalUnit(unit);
+
+		if (action == -1){
+			addLexicalUnit(unit, action, NonTerminal, IDNTER);
+		}else if (action == -2){
+			lexicalErrors.push_back(unit);
+		}else{
+			addLexicalUnit(rmActionFromLexicalUnit(unit), action, NonTerminal, IDNTER);
+		}
+
+	}else if(isNumber(unit)){
+		addLexicalUnit(unit, -1, Terminal, ELTER);
+	}else{
+		lexicalErrors.push_back(unit);
+	}
+}
+}
+
+// previous version of Scanner
+
+// void scanner(string text){
+
+// 	unsigned int i;
+
+// 	string unit = "";
+// 	char currentChar;
+// 	string doubleSymbol;
+
+// 	for(i = 0 ; i < text.size() ; ++i){
+// 		currentChar = text.at(i);
+// 		if(isSymbol(currentChar) || isSeparator(currentChar)){
+// 			if (unit != "") {
+// 				if (!isInteger(unit.at(0))){
+// 					lexicalUnits.push_back(unit);
+// 				}else if(isNumber(unit)){
+// 					lexicalUnits.push_back(unit);
+// 				}else{
+// 					lexicalErrors.push_back(unit);
+// 				}
+// 			}
+// 			unit = "";
+
+// 			if (isPotentialySymbolDouble(currentChar) && (i != text.size()-1)){
+// 				if (isSymbolDouble(currentChar, text.at(i+1))){
+// 					doubleSymbol = currentChar;
+// 					doubleSymbol += text.at(i+1);
+// 					symbols.push_back(doubleSymbol);
+// 					++i;
+// 				}else{
+// 					symbols.push_back(string(1,currentChar)); // Cast Char to String				}
+// 				}
+// 			}else if (isSymbol(currentChar)){
+// 				symbols.push_back(string(1,currentChar)); // Cast Char to String
+
+// 			}else if (isSeparator(currentChar)){
+// 				separators.push_back(currentChar);
+// 			} // TO DO -> Gérer les actions
+// 		}else{
+// 			unit += currentChar;
+// 		}
+// 	}
+// 	// Add the last unit found
+// 	lexicalUnits.push_back(unit);
+// }
+
+void scanner (string text){
 
 	string unit = "";
 	char currentChar;
-	string doubleSymbol;
 
-	for(i = 0 ; i < text.size() ; ++i){
+	for(int i = 0 ; i < text.size() ; ++i){
 		currentChar = text.at(i);
+
 		if(isSymbol(currentChar) || isSeparator(currentChar)){
-			if (unit != "") {
-				if (!isInteger(unit.at(0))){
-					lexicalUnits.push_back(unit);
-				}else if(isNumber(unit)){
-					lexicalUnits.push_back(unit);
-				}else{
-					lexicalErrors.push_back(unit);
-				}
-			}
+			analyseUnit(unit);
 			unit = "";
 
-			if (isPotentialySymbolDouble(currentChar) && (i != text.size()-1)){
-				if (isSymbolDouble(currentChar, text.at(i+1))){
-					doubleSymbol = currentChar;
-					doubleSymbol += text.at(i+1);
-					symbols.push_back(doubleSymbol);
-					++i;
-				}else{
-					symbols.push_back(string(1,currentChar)); // Cast Char to String				}
-				}
-			}else if (isSymbol(currentChar)){
-				symbols.push_back(string(1,currentChar)); // Cast Char to String
-
-			}else if (isSeparator(currentChar)){
-				separators.push_back(currentChar);
-			} // TO DO -> Gérer les actions
 		}else{
 			unit += currentChar;
 		}
 	}
-	// Add the last unit found
-	lexicalUnits.push_back(unit);
+
+	analyseUnit(unit); // analyse the last unit found
 }
 
 void printLexicalUnits(){
 	cout << "List of lexical units :" << endl;
 	for (int i = 0; i < lexicalUnits.size(); ++i){
-		cout << lexicalUnits[i] << endl;
+		cout << "----------------" << endl;
+
+		cout << "Chaine : " << lexicalUnits[i].chaine << endl;
+		cout << "Action : " << lexicalUnits[i].action << endl;
+		cout << "type : " << lexicalUnits[i].type << endl;
+		cout << "Code : " << lexicalUnits[i].code << endl;
+
 	}
 }
 
-void printSeparators(){
-	cout << "List of separators :" << endl;
-	for (int i = 0; i < separators.size(); ++i){
-		cout << i+1 << separators[i] << endl;
-	}
-}
+// void printSeparators(){
+// 	cout << "List of separators :" << endl;
+// 	for (int i = 0; i < separators.size(); ++i){
+// 		cout << i+1 << separators[i] << endl;
+// 	}
+// }
 
-void printSymbols(){
-	cout << "List of symbols :" << endl;
-	for (int i = 0; i < symbols.size(); ++i){
-		cout << symbols[i] << endl;
-	}
-}
+// void printSymbols(){
+// 	cout << "List of symbols :" << endl;
+// 	for (int i = 0; i < symbols.size(); ++i){
+// 		cout << symbols[i] << endl;
+// 	}
+// }
 
 void printLexicalErrors(){
 	cout << "List of lexical errors :" << endl;
@@ -357,22 +464,15 @@ void printLexicalErrors(){
 	}
 }
 
-void printActions(){
-	cout << "List of actions :" << endl;
-	for (int i = 0; i < actions.size(); ++i){
-		cout << actions[i] << endl;
-	}
-}
-
 int main(){
 
-	printForest();
+	// printForest();
 
-	scanner("1coucou rioe54erer 654654 poney=<==++++&&&loul");
+	scanner("1coucou \"rioe54ere\" ioehrz#2 654654 iorue#23ko");
 
 	printLexicalUnits();
-	printSymbols();
-	printSeparators();
+	// printSymbols();
+	// printSeparators();
 	printLexicalErrors();
 
 	return 0;
